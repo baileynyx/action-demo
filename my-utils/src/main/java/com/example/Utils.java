47efab3cc108d3016041a37c690e58e7b334de62
BaileyNyx <bailey.fitchett@xebia.com>
2024-09-11 14:11:47 -0400

    update build.yml

---
+++ my-utils/src/main/java/com/example/Utils.java
@@ -0,0 +1,12 @@
+// src/main/java/com/example/Utils.java
+package com.example;
+
+public class Utils {
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(greet("World"));
+    }
+}
